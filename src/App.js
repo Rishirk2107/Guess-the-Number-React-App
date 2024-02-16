b0cc4eb322453dbcc218afa496b1041316e0fa12
Rishirk2107 <120192740+Rishirk2107@users.noreply.github.com>
2024-02-16 11:28:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home'
-import About from './components/About';
-import Contact from './components/Contact';
-import Profile from './components/Profile'
-import { BrowserRouter,Routes,Route,Link} from 'react-router-dom';
+import {useState} from 'react';
+import Result from './Result';
+const secretNum=Math.floor(Math.random()*10) + 1;
 
 function App() {
+  const [term,setTerm]=useState("");
+  const handleChange=(e)=>{
+    setTerm(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <BrowserRouter>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact Us</Link></li>
-        <li><Link to="/user/1">User 1 Profile</Link></li>
-        <li><Link to="/user/2">User 2 Profile</Link></li>
-      </ul>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/user/:id' element={<Profile/>}/>
-      </Routes>
-      </BrowserRouter>
+    <div className="container">
+        <div className='head'>
+          <label>Guess the number between 1 to 10</label>
+        </div>
+        <input
+          type='text'
+          name="term"
+          onChange={handleChange}
+          />
+          <Result
+          secretNum={secretNum}
+          term={term}/>
     </div>
   );
 }
